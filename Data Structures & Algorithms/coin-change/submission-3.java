423cbe1eb4d694b89df33b55cbc05670062fef15
Arghyadeep Barat <41644474+BaratArghyadeep@users.noreply.github.com>
2026-09-09 09:30:25 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for(int i = 0; i<n; i++)
+        {
+            Arrays.fill(dp[i] , -1);
+        }
+        int ans =  minimumCoin(n-1 , coins , amount , dp);
+
+        return ans >= 1e9 ? -1 : ans;
+    }
+
+    public int minimumCoin(int idx , int[] coins , int target , int[][] dp)
+    {
+
+        if(idx == 0)
+        {
+            if(target % coins[idx] == 0)
+            {
+                return target / coins[idx];
+            }
+
+            return (int) 1e9;
+        }
+        if(dp[idx][target] != -1)
+        {
+            return dp[idx][target];
+        }
+
+
+
+        int notTake = minimumCoin(idx-1 , coins , target , dp);
+        int take = (int) 1e9;
+        if(coins[idx] <= target)
+        {
+            take = 1 + minimumCoin(idx , coins , target - coins[idx] , dp);
+        }
+        dp[idx][target] = Math.min(take,notTake);
+        return Math.min(take , notTake);
+    }
+}
